e3b80c6e992ceee7cc680c3f8585502bbaf19325
Yulia Glagola <julichis@mail.ru>
2024-06-22 16:59:04 +0300

    installed sass, vite-plugin-svgr, embedded svg as react components

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,21 +1,35 @@
 import React from "react";
-import logo from "../../assets/logo.svg";
-import bookmarkIcon from "../../assets/icon-nav-bookmark.svg";
-import homeIcon from "../..//assets/icon-nav-home.svg";
-import moviesIcon from "../..//assets/icon-nav-movies.svg";
-import tvSeriesIcon from "../..//assets/icon-nav-tv-series.svg";
+import Logo from "../../assets/logo.svg?react";
+import BookmarkIcon from "../../assets/icon-nav-bookmark.svg?react";
+import HomeIcon from "../../assets/icon-nav-home.svg?react";
+import MoviesIcon from "../../assets/icon-nav-movies.svg?react";
+import TvSeriesIcon from "../../assets/icon-nav-tv-series.svg?react";
+import avatar from "../../assets/image-avatar.png"
+import styles from "./Navbar.module.scss";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="navbar_logo"><img src={logo} alt="" /></div>
-      <div className="navbar__links-group">
-        <a href="" className="navbar__item"><img src={homeIcon} alt="" /></a>
-        <a href="" className="navbar__item"><img src={moviesIcon} alt="" /></a>
-        <a href="" className="navbar__item"><img src={tvSeriesIcon} alt="" /></a>
-        <a href="" className="navbar__item"><img src={bookmarkIcon} alt="" /></a>
+    <nav className={styles.navbar}>
+      <div className={styles.navbar__logo}>
+        <Logo />
       </div>
-      <a href="" className="navbar__avatar"><img src="" alt="" className="navbar__avatar-img" /></a>
+      <div className={styles.navbar__links}>
+        <a href="" className={styles.navbar__item}>
+          <HomeIcon />
+        </a>
+        <a href="" className={styles.navbar__item}>
+          <MoviesIcon />
+        </a>
+        <a href="" className={styles.navbar__item}>
+          <TvSeriesIcon />
+        </a>
+        <a href="" className={styles.navbar__item}>
+          <BookmarkIcon />
+        </a>
+      </div>
+      <a href="" className={styles.navbar__avatar}>
+        <img src={avatar} alt="" className="navbar__avatar-img" />
+      </a>
     </nav>
   );
 };
