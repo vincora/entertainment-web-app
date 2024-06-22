66aa663d8a810b01f5722a965fb370d3728e5a2a
Yulia Glagola <julichis@mail.ru>
2024-06-22 20:46:27 +0300

    app layout and navbar styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import './App.css'
-import Navbar from './components/navbar/Navbar'
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Searchbar from "./components/search/Searchbar";
 
 function App() {
-
   return (
-    <>
-    <Navbar/>
-    <div>searchbar</div>
-    <div className="">page content</div>
-    </>
-  )
+    <div className="app">
+      <Navbar />
+      <div className="layout">
+        <Searchbar />
+        <div className="">page content</div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
