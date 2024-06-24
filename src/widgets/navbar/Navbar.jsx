4e85c7ab53c19594bc58a9370ce42e70a31b400c
Yulia Glagola <julichis@mail.ru>
2024-06-24 21:33:32 +0300

    added active state mod for nav links

--- src/widgets/navbar/Navbar.jsx
+++ src/widgets/navbar/Navbar.jsx
@@ -6,25 +6,29 @@ import MoviesIcon from "../../assets/icon-nav-movies.svg?react";
 import TvSeriesIcon from "../../assets/icon-nav-tv-series.svg?react";
 import avatar from "../../assets/image-avatar.png";
 import styles from "./Navbar.module.scss";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import cn from "clsx";
 
 const Navbar = () => {
+
+  const location = useLocation();
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.navbar__logo}>
         <Logo />
       </div>
       <div className={styles.navbar__links}>
-        <Link to={"/"} className={styles.navbar__item}>
+        <Link to={"/"} className={cn(styles.navbar__item, {[styles.navbar__item_active]: location.pathname === '/'})}>
           <HomeIcon />
         </Link>
-        <Link to={"/movies"} className={styles.navbar__item}>
+        <Link to={"/movies"} className={cn(styles.navbar__item, {[styles.navbar__item_active]: location.pathname === '/movies'})}>
           <MoviesIcon />
         </Link>
-        <Link to={"/tv-series"} className={styles.navbar__item}>
+        <Link to={"/tv-series"} className={cn(styles.navbar__item, {[styles.navbar__item_active]: location.pathname === '/tv-series'})}>
           <TvSeriesIcon />
         </Link>
-        <Link to={"/bookmarked"} className={styles.navbar__item}>
+        <Link to={"/bookmarked"} className={cn(styles.navbar__item, {[styles.navbar__item_active]: location.pathname === '/bookmarked'})}>
           <BookmarkIcon />
         </Link>
       </div>
