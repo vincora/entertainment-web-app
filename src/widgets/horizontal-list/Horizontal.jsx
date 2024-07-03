c838bb7695a14ce9c0098bd83f5816461b1a1a49
Yulia Glagola <julichis@mail.ru>
2024-07-03 22:27:21 +0300

    styles refactoring

--- src/widgets/horizontal-list/Horizontal.jsx
+++ src/widgets/horizontal-list/Horizontal.jsx
@@ -4,22 +4,10 @@ import ContentItem from "../../components/content/ContentItem";
 
 const Horizontal = ({ data }) => {
 
-  // const scrollRef = useRef(null);
-  // const handleWheel = (e) => {
-  //   if (e.deltaY !== 0) {
-  //     e.preventDefault();
-  //     scrollRef.current.scrollLeft += e.deltaY;
-  //   }
-  // };
-
   return (
-    <div className={styles.horizontal}>
+    <div className={styles.horizontal} >
       <h1 className={styles.horizontal__title}>Trending</h1>
-      <ul
-        className={styles.horizontal__list}
-        // ref={scrollRef}
-        // onWheel={handleWheel}
-      >
+      <ul className={styles.horizontal__list}>
         {data.map(
           ({ title, year, category, rating, thumbnail, isTrending }) => {
             return (
